e62033c5dac5ce4daa5b1ed538de18ff258e3ccb
Moon <roblozgg@gmail.com>
2026-08-25 00:43:40 +0300

    Удаление всех файлов

--- lib/config.ts
+++
@@ -1,8 +0,0 @@
-export const APP_NAME = "Moon";
-export const APP_DESCRIPTION = `${APP_NAME} web messenger`;
-export const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
-export function withBasePath(path: string) {
-  const normalized = path.startsWith("/") ? path : `/${path}`;
-  return `${BASE_PATH}${normalized}`;
-}
